6c4aa277b97aea163bbfaf86abf8cca9e331bf97
AnnDeUm <47113574+AnnDeUm@users.noreply.github.com>
2019-05-03 11:36:41 +0800

    Add files via upload

---
+++ histogram.R
@@ -0,0 +1,5 @@
+setwd("C:/Users/Ann Umadhay/Documents/UP ANN S14/4th year 2nd sem/Stat197/3rd activity/Ann Umadhay - CourseProject3-data")
+outcome <- read.csv("outcome-of-care-measures.csv", colClasses="character")
+head(outcome)
+outcome[,11] <- as.numeric(outcome[,11])
+hist(outcome[,11], xlab = "30-day Death Rate", main = "Heart Attack 30-day Death Rate")
