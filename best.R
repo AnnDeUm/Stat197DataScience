7bfc0e5b8a37deac751dbff6419f671ad9a7a0c6
AnnDeUm <47113574+AnnDeUm@users.noreply.github.com>
2019-05-03 12:11:11 +0800

    Add files via upload

--- best.R
+++ best.R
@@ -9,14 +9,13 @@ Init <- function(fileStr, workDirStr = "C:/Users/Ann Umadhay/Documents/UP ANN S1
 best <- function(stateChr, outcomeChr) {
  outcomeDfr <- Init("C:/Users/Ann Umadhay/Documents/UP ANN S14/4th year 2nd sem/Stat197/3rd activity/Ann Umadhay - CourseProject3-data/outcome-of-care-measures.csv")
 
- suppressWarnings(outcomeDfr[, 11] <- as.numeric(outcomeDfr[, 11]))
- suppressWarnings(outcomeDfr[, 17] <- as.numeric(outcomeDfr[, 17]))
- suppressWarnings(outcomeDfr[, 23] <- as.numeric(outcomeDfr[, 23]))
- 
+ suppressWarnings(outcomeDfr[, 11] <- as.numeric(outcomeDfr[, 11])) #Hospital 30-Day Death (Mortality) Rates from Heart Attack
+ suppressWarnings(outcomeDfr[, 17] <- as.numeric(outcomeDfr[, 17])) #Footnote - Hospital 30-Day Death (Mortality) Rates from Heart Attack
+ suppressWarnings(outcomeDfr[, 23] <- as.numeric(outcomeDfr[, 23])) #Number of Patients - Hospital 30-Day Death (Mortality) Rates from Heart Failure
+
  tableDfr <- data.frame(State = names(tapply(outcomeDfr$State, outcomeDfr$State, 
                                              length)), Freq = tapply(outcomeDfr$State, outcomeDfr$State, length))
  rownames(tableDfr) <- NULL
- 
  inputDfr <- data.frame(Outcome = c("heart attack", "heart failure", "pneumonia"), 
                         Col = c(11, 17, 23))
  
@@ -24,23 +23,15 @@ best <- function(stateChr, outcomeChr) {
   stop("invalid state")
  if (nrow(inputDfr[inputDfr$Outcome == outcomeChr, ]) == 0) 
   stop("invalid outcome")
-
+ # --- Return hospital name in that state with lowest THIRTY(30)-day death
+ # rate Create a data frame with given ONE (1) state Determine the relevant
+ # row and column
  stateDfr <- outcomeDfr[outcomeDfr$State == stateChr, ]
  colNum <- inputDfr[inputDfr$Outcome == outcomeChr, 2]
  rowNum <- which.min(stateDfr[, colNum])
  return(stateDfr[rowNum, ]$Hospital.Name)
 }
 
-freqVtr <- function(inDfr, orderVtr) {
- outVtr <- numeric(0)
- 
- for (ord in orderVtr) {
-  outVtr <- c(outVtr, inDfr[inDfr$State == ord, 2])
- }
- 
- return(outVtr)
-}
-source("best.R")
 best("TX", "heart failure")
 best("MD", "heart attack")
 best("MD", "pneumonia")
